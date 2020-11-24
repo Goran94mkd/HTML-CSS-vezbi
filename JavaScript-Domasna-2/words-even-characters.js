0a578a942c8f87cee7625ae552f4c184707d7aa5
Goran94mkd <71820868+Goran94mkd@users.noreply.github.com>
2020-11-24 04:10:24 +0100

    JavaScript-domasna-2

---
+++ JavaScript-Domasna-2/words-even-characters.js
@@ -0,0 +1,27 @@
+const string = 'I am a student in Computer Science and JavaScript academy at Semos Education. I am taking a course for JavaScript currently. My goals are to become a Software Developer after that, so I am working toward it right now.'
+
+const secondString = string.split(' ');
+console.log(secondString); 
+
+function arrFunction(arr) {
+	let array = [];
+	for (let i = 0; i < arr.length; i++) {
+		  if (arr[i].length % 2 === 0) {
+		  array.push(arr[i]);
+		  }
+	}
+	return array;
+}
+console.log(arrFunction(secondString));
+
+const evenStrings = arrFunction(secondString);
+const obj = Object();
+
+for (let i = 0; i < evenStrings.length; i++) {
+	if(obj[evenStrings[i]] != null) {
+		obj[evenStrings[i]] += 1;
+	} else {
+		obj[evenStrings[i]] = 1;
+	}
+}
+console.log(obj); 
