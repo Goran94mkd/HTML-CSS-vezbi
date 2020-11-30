8397cbc27179793c8b7b6572f135ef10e4b8082c
Goran94mkd <71820868+Goran94mkd@users.noreply.github.com>
2020-11-30 19:59:37 +0100

    Add files via upload

---
+++ vezba3.js
@@ -0,0 +1,46 @@
+// Regular
+
+const string3 = "j97va11599ri112116";
+const numbers = string3.split('').filter(number => number > 0).join(' ').replaceAll(' ', '')
+const letters = string3.split('').filter(char => /[a-z]/.test(char)).join().replaceAll(',', '')
+// console.log(letters) 
+// console.log(numbers) 
+
+const asciiToChar = function (func) {
+  const ascii = [];
+  const arr = [];
+  const a = charToAscii(letters).substr(0,3); // 106
+  const c = charToAscii(letters).substr(3,5); // 11897
+  const f = charToAscii(letters).substr(8,11); // 114105
+  const b = convertedString2.substr(0,1) // a
+  const d = convertedString2.substr(1,2) // sc
+  const e = convertedString2.substr(3,4) // pt
+
+  for (let i = 0; i < numbers.length;) {
+    const num = numbers[i] === '1' ? 3 : 2;
+    ascii.push(numbers.substr(i, num));
+    i += num;
+    arr.join('')
+  } 
+
+  for (let i = 0; i < text.length; i++){
+    const lettersPush = text.charCodeAt(i);
+    arr.push(lettersPush);
+    const convertedString2 = String.fromCharCode(...ascii);} // ascpt
+    // console.log(convertedString2)
+    arr.join('')
+ return {
+   ascii,
+   arr
+ }
+} 
+console.log(asciiToChar); // 10611897114105
+
+
+
+
+
+
+
+
+
